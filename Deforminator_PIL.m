d48ea5ea0c4fb8ea805df69fae95f1223b112bf4
ruicarsa <rui.carlos.sa@gmail.com>
2019-04-25 12:09:18 -0700

    MRDC (standard) version

--- Deforminator_PIL.m
+++ Deforminator_PIL.m
@@ -54,8 +54,8 @@ if nargin == 0
 
     %Load all dicom images in dire2
     %[IM_unreg]=LoadAllDicomFiles(dire2,'MRDC.');
-    %[IM_unreg]=LoadAllDicomFiles(dire2,'MRDC.');
-    [IM_unreg]=LoadAllDicomFiles(dire2,'IM_');
+    [IM_unreg]=LoadAllDicomFiles(dire2,'MRDC.');
+    %[IM_unreg]=LoadAllDicomFiles(dire2,'IM_');
     cd(home);
     Projective_deformation_GUI_Vol3([],[],IM_unreg,[],saveFilename,path2save,loadedpath);
 
